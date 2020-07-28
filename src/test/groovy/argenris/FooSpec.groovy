633e928bdb17ec950f2c3ca25d8df25d5402a998
jose <jmrodriguez@fi.uba.ar>
2020-07-28 01:56:51 -0300

    prueba inicial travis

---
+++ src/test/groovy/argenris/FooSpec.groovy
@@ -0,0 +1,18 @@
+package argenris
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class FooSpec extends Specification implements DomainUnitTest<Foo> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == true
+    }
+}
