6526f2c0b8b24981097d5f30ed0d63170fcdcf74
jose <jmrodriguez@fi.uba.ar>
2020-09-13 03:46:35 -0300

    Hello React!

---
+++ server/grails-app/domain/argenris/AreaDeExamenDummy.groovy
@@ -0,0 +1,24 @@
+package argenris
+
+import argenris.AreaDeExamen
+import argenris.Cita.Cita
+import argenris.OrdenDeEstudio.OrdenDeEstudio
+
+import java.time.LocalDateTime
+
+class AreaDeExamenDummy extends AreaDeExamen{
+
+    @Override
+    List<Cita> obtenerCitasDelDia(LocalDateTime dia) {
+        null
+    }
+
+    Cita crearCita(LocalDateTime fechaYHoraCita, String prioridad, OrdenDeEstudio unaOrden) {
+        super.crearCita(fechaYHoraCita ,prioridad, unaOrden)
+    }
+
+    @Override
+    boolean puedoAgregarCita(LocalDateTime fechaCita) {
+        false
+    }
+}
